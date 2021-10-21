be4ccea5f920d6a2cbb325894f8d5eb877e0a31b
kyuho <dlrbgh9303@gmail.com>
2021-10-21 16:13:53 +0900

    "LoginFrame 수정"

--- src/main/LoginFrame.java
+++ src/main/LoginFrame.java
@@ -19,129 +19,144 @@ import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 import dao.UserDAO;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
 
 public class LoginFrame extends JFrame {
 
-	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+   private JPanel contentPane;
+   private JTextField textField_1;
+   private JTextField textField_2;
 
-	private JButton btnLogin;
-	private JButton btnjoin;
+   private JButton btnLogin;
+   private JButton btnjoin;
 
-	public JButton getBtnLogin() {
-		return btnLogin;
-	}
+   public JButton getBtnLogin() {
+      return btnLogin;
+   }
 
-	public JButton getBtnjoin() {
-		return btnjoin;
-	}
+   public JButton getBtnjoin() {
+      return btnjoin;
+   }
 
-	public void setBtnLogin(JButton btnLogin) {
-		this.btnLogin = btnLogin;
-	}
+   public void setBtnLogin(JButton btnLogin) {
+      this.btnLogin = btnLogin;
+   }
 
-	public void setBtnjoin(JButton btnjoin) {
-		this.btnjoin = btnjoin;
-	}
+   public void setBtnjoin(JButton btnjoin) {
+      this.btnjoin = btnjoin;
+   }
 
-	UserDAO dao = new UserDAO();
+   UserDAO dao = new UserDAO();
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					LoginFrame frame = new LoginFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+   /**
+    * Launch the application.
+    */
+   public static void main(String[] args) {
+      EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            try {
+               LoginFrame frame = new LoginFrame();
+               frame.setVisible(true);
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
+         }
+      });
+   }
 
-	/**
-	 * Create the frame.
-	 */
-	public LoginFrame() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage(LoginFrame.class.getResource("/main/movie.png")));
-		setTitle("JAVA CINEMA-로그인");
-		setTitle("JAVA CINEMA 로그인");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 350);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+   /**
+    * Create the frame.
+    */
+   public LoginFrame() {
+      setIconImage(Toolkit.getDefaultToolkit().getImage(LoginFrame.class.getResource("/main/movie.png")));
+      setTitle("JAVA CINEMA-로그인");
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setBounds(100, 100, 450, 350);
+      contentPane = new JPanel();
+      
+      setContentPane(contentPane);
+      contentPane.setLayout(new BorderLayout(0, 0));
 
-		textField = new JTextField();
-		textField.setFont(new Font("돋움", Font.BOLD, 20));
-		textField.setHorizontalAlignment(SwingConstants.CENTER);
-		textField.setText("\uC790\uBC14\uC2DC\uB124\uB9C8 \uC628\uB77C\uC778 \uC601\uD654 \uC608\uC57D");
-		textField.setBounds(0, 0, 436, 83);
-		contentPane.add(textField);
-		textField.setColumns(10);
+      JPanel panel = new JPanel();
+      panel.setBorder(new EmptyBorder(10, 0, 10, 0));
+      panel.setBackground(Color.WHITE);
+      contentPane.add(panel, BorderLayout.NORTH);
+      
+      JLabel lblNewLabel_1 = new JLabel("자바시네마 온라인 예매");
+      lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+      lblNewLabel_1.setFont(new Font("휴먼엑스포", Font.BOLD, 25));
+      panel.add(lblNewLabel_1);
+      
+      JPanel panel_1 = new JPanel();
+      FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
+      flowLayout.setHgap(30);
+      panel_1.setBorder(new EmptyBorder(5, 0, 25, 0));
+      contentPane.add(panel_1, BorderLayout.SOUTH);
+      
+      JPanel panel_2 = new JPanel();
+      contentPane.add(panel_2, BorderLayout.CENTER);
+      panel_2.setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("ID");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("돋움", Font.BOLD, 15));
-		lblNewLabel.setBounds(81, 106, 124, 37);
-		contentPane.add(lblNewLabel);
+      JLabel lblNewLabel = new JLabel("ID");
+      lblNewLabel.setBounds(79, 27, 92, 45);
+      lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+      lblNewLabel.setFont(new Font("휴먼엑스포", Font.BOLD, 18));
+      panel_2.add(lblNewLabel);
 
-		JLabel lblPw = new JLabel("PW");
-		lblPw.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPw.setFont(new Font("돋움", Font.BOLD, 15));
-		lblPw.setBounds(81, 167, 124, 37);
-		contentPane.add(lblPw);
+      JLabel lblPw = new JLabel("PW");
+      lblPw.setBounds(66, 87, 117, 52);
+      lblPw.setHorizontalAlignment(SwingConstants.CENTER);
+      lblPw.setFont(new Font("휴먼엑스포", Font.BOLD, 18));
+      panel_2.add(lblPw);
 
-		textField_1 = new JTextField();
-		textField_1.setBounds(217, 106, 124, 37);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+      textField_1 = new JTextField();
+      textField_1.setBounds(188, 32, 150, 35);
+      panel_2.add(textField_1);
+      textField_1.setColumns(10);
 
-		textField_2 = new JPasswordField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(217, 167, 124, 37);
-		contentPane.add(textField_2);
+      textField_2 = new JPasswordField();
+      textField_2.setBounds(188, 96, 150, 35);
+      textField_2.setColumns(10);
+      panel_2.add(textField_2);
 
-		JButton btnNewButton = new JButton("취소");
-		btnNewButton.setBounds(228, 230, 112, 23);
-		contentPane.add(btnNewButton);
-		btnNewButton.addActionListener(new ActionListener() {
 
-			public void actionPerformed(ActionEvent e) {
-				new MainFrame().setVisible(true); // 조회화면 띄우기
-				setVisible(false);
-			}
-		});
+      JButton btnNewButton_1 = new JButton("\uB85C\uADF8\uC778");
+      btnNewButton_1.setFont(new Font("휴먼엑스포", Font.PLAIN, 15));
+      panel_1.add(btnNewButton_1);
+      
+      btnNewButton_1.addActionListener(new ActionListener() {
 
-		JButton btnNewButton_1 = new JButton("\uB85C\uADF8\uC778");
-		btnNewButton_1.setBounds(81, 230, 112, 23);
-		contentPane.add(btnNewButton_1);
-		btnNewButton_1.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent e) {
-				String userid = textField_1.getText();
-				String userpwd = textField_2.getText();
+         public void actionPerformed(ActionEvent e) {
+            String userid = textField_1.getText();
+            String userpwd = textField_2.getText();
 //            UserDTO dto = new UserDTO(userid,userpwd);
-				if (dao.getRow(userid, userpwd) != null) {
-					JOptionPane.showMessageDialog(null, "로그인성공\n" + userid + "님 환영합니다.");
-					MainFrame f = new MainFrame();
-					f.getBtnLogin().setVisible(false);
-					f.getBtnjoin().setVisible(false);
-					f.setUserid(userid);
-					f.setVisible(true);
-					setVisible(false);
-				} else {
-					JOptionPane.showMessageDialog(null, "로그인 실패");
-				}
-				System.out.println();
+            if (dao.getRow(userid, userpwd) != null) {
+               JOptionPane.showMessageDialog(null, "로그인성공\n" + userid + "님 환영합니다.");
+               MainFrame f = new MainFrame();
+               f.getBtnLogin().setVisible(false);
+               f.getBtnjoin().setVisible(false);
+               f.setUserid(userid);
+               f.setVisible(true);
+               setVisible(false);
+            } else {
+               JOptionPane.showMessageDialog(null, "로그인 실패");
+            }
+            System.out.println();
 
-			}
-		});
-	}
+         }
+      });
+      JButton btnNewButton = new JButton("취소");
+      btnNewButton.setFont(new Font("휴먼엑스포", Font.PLAIN, 15));
+      panel_1.add(btnNewButton);
+      btnNewButton.addActionListener(new ActionListener() {
+         
+         public void actionPerformed(ActionEvent e) {
+            new MainFrame().setVisible(true); // 조회화면 띄우기
+            setVisible(false);
+         }
+      });
+   }
 }
